73e2aea772ac9ca4ad5b622f9a2f08185bc9ac87
sbpolicar <sbpolicar@gmail.com>
2015-07-20 19:19:12 -0700

    User model test good

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+
+  validates :email,
+  presence: true,
+  uniqueness: {case_sensitive:false}
+
+  validates_presence_of :password, on: :create
+
+  has_many :pets
+
+  def self.authenticate email, password
+    User.find_by_email(email).try(:authenticate, password)
+
+  end
+
+
 end
